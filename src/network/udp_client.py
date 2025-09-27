d19ef5e1d960b9aceb34734a02747339e41ba094
Carlos Duarte-Molina <carlosduarte.molina97@gmail.com>
2025-09-27 18:56:24 -0400

    Now auto-discovering device

--- src/network/udp_client.py
+++ src/network/udp_client.py
@@ -2,17 +2,40 @@ import socket
 import struct
 from threading import Lock
 
+from threading import Thread
+import time
+from time import sleep
+
+import logging
+
 
 class UDPCLient:
 
     CAR_STATIC_IP = "192.168.1.10"
-    PORT = 57345
+    PORT = 65000
 
     def __init__(self) -> None:
         self.__socket_mutex = Lock()
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__server_ip : str = ""
         self.__timeout : int = 5  # 5 seconds default timeout
+        thread = Thread(target=self.__search_hostname)
+        thread.daemon = True
+        thread.start()
+
+
+    def __search_hostname(self) -> None:
+        """
+        Hostname search service
+        """
+        while True:
+            ip = socket.gethostbyname("rc-car-machine.local")
+            if len(ip) > 0:
+                self.__server_ip = ip
+                logging.info("Found RC car at address %s", self.__server_ip)
+                break
+            
+            time.sleep(5)
 
 
     def set_timeout(self, timeout: float) -> None:
@@ -39,17 +62,16 @@ class UDPCLient:
                 - FALSE: Failed to transmit data
         """
         try:
-            self.__socket.sendto( data, (UDPCLient.CAR_STATIC_IP, UDPCLient.PORT ) )
+            self.__socket.sendto( data, (self.__server_ip, UDPCLient.PORT ) )
         except:
             return False
         
         return True
 
 
-    def receive_data( self ) -> bytes:
-        data: bytes = self.__socket.recvfrom(4096)
-        if data == None:
-            return b''
-        
-        return data
-
+    def receive_data(self) -> bytes | None:
+        try:
+            data, addr = self.__socket.recvfrom(4096)  # no flags on Windows
+            return data
+        except TimeoutError:
+            return None
